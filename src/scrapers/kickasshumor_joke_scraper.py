4c4713b5bb6e18000fb7054447c56f5f5ba899ce
raholm <rasmus.holm723@gmail.com>
2017-11-28 22:17:51 +0100

    Created a kickasshumor.com scraper.

---
+++ src/scrapers/kickasshumor_joke_scraper.py
@@ -0,0 +1,123 @@
+import json
+
+import time
+
+import os
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+
+from src.util.env import get_env_variable
+
+
+class KickassHumorJokeScraper(object):
+	def __init__(self):
+		self.jokes = {}
+
+		self.click_limit = 5
+		self.category = None
+
+	def scrape(self, start_url):
+		driver = webdriver.Firefox()
+		driver.get(start_url)
+
+		driver.find_element_by_xpath("//a[@title='Best Jokes of All Time']").click()
+
+		self.category = self._get_category(driver)
+
+		while self._is_more_jokes(driver):
+			self._load_more_jokes(driver)
+
+		self._get_jokes(driver)
+		self._write_jokes_to_file()
+
+		return self.jokes
+
+	def _get_jokes(self, driver):
+		for joke in driver.find_elements_by_xpath("//div[@class='joke cfix expand']"):
+			id = self._get_id(joke)
+
+			if id in self.jokes:
+				continue
+
+			self._load_more_text(joke, self.click_limit)
+
+			punchline = self._get_punchline(joke)
+
+			self.jokes[id] = {"id": id,
+							  "punchline": punchline,
+							  "category": self.category}
+
+	def _is_more_jokes(self, driver):
+		try:
+			driver.find_element_by_xpath(
+				"//div[@id='jokes']/p").get_attribute("text")
+			return False
+		except NoSuchElementException:
+			return True
+
+	def _load_more_jokes(self, driver):
+		driver.find_element_by_xpath("//div[@class='loadMore']/a").click()
+
+	def _get_id(self, joke):
+		return joke.get_attribute("data-jokeid")
+
+	def _get_punchline(self, joke):
+		return joke.find_element_by_xpath("./p/a").get_attribute("text")
+
+	def _get_category(self, driver):
+		return driver.current_url.split("/")[-1]
+
+	def _write_jokes_to_file(self):
+		file = get_env_variable("TMP_DATA_PATH") + "/kickasshumor_raw/%s.json" % self.category.replace("-", "_")
+
+		with open(file, "w", encoding="utf-8") as outfile:
+			json.dump(self.jokes, outfile, indent=4, sort_keys=True)
+
+	def _load_more_text(self, joke, limit):
+		try:
+			joke.find_element_by_xpath(".//span[@class='read-more']/a").click()
+			time.sleep(0.15)
+			if joke.find_element_by_xpath(".//span[@class='read-more']").is_displayed():
+				self._load_more_text(joke, limit - 1)
+		except NoSuchElementException:
+			pass
+
+
+def main():
+	id_to_category = {1: "funny_chuck_norris_jokes",
+					  2: "funny_yo_momma_jokes",
+					  3: "funny_blonde_jokes",
+					  4: "funny_one_liner_jokes",
+					  5: "funny_short_jokes",
+					  6: "funny_long_jokes",
+					  7: "funny_redneck_jokes",
+					  9: "funny_dirty_jokes",
+					  10: "funny_racial_jokes",
+					  12: "funny_comebacks",
+					  14: "funny_pick_up_lines",
+					  15: "funny_celebrity_jokes",
+					  16: "funny_anti_humor_jokes",
+					  17: "funny_animal_jokes",
+					  18: "funny_puns"}
+
+	scraped_categories = {category.split(".")[0]
+						  for category in os.listdir(get_env_variable("TMP_DATA_PATH") + "/kickasshumor_raw")}
+
+	category_ids = [id for id, category in id_to_category.items()
+					if category not in scraped_categories]
+
+	base_url = "https://www.kickasshumor.com/c/%i"
+	urls = [base_url % id for id in category_ids]
+
+	print(urls)
+
+# start_url = "https://www.kickasshumor.com/c/7"
+#
+# scraper = KickassHumorJokeScraper()
+# jokes = scraper.scrape(start_url)
+#
+# print(len(jokes))
+
+
+if __name__ == "__main__":
+	main()
--- src/scrapers/reddit_joke_scraper.py
+++ src/scrapers/reddit_joke_scraper.py
@@ -1,9 +1,12 @@
 import json
 import time
 
+import os
 from joblib import Parallel, delayed
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+
+from src.util.env import get_env_variable
 
 
 class RedditJokeScraper(object):
@@ -34,11 +37,11 @@ class RedditJokeScraper(object):
 		start = time.time()
 
 		while pages_accessed < self.num_pages:
-			for (premise, punchline), domain in self._get_jokes(driver):
+			for id, (premise, punchline), domain in self._get_jokes(driver):
 				if premise is None and punchline is None:
 					continue
 
-				jokes.append((premise, punchline, domain))
+				jokes.append((id, premise, punchline, domain))
 
 			next_page = self._get_next_page(driver)
 
@@ -54,10 +57,14 @@ class RedditJokeScraper(object):
 		driver.close()
 
 		print(start_url)
+		print("Total number of pages:", pages_accessed + 1)
 		print("Total time in seconds:", end - start)
 
 		if pages_accessed > 0:
 			print("Average time per page in seconds:", (end - start) / pages_accessed)
+
+		filename = start_url.split("/")[-1] + ".json"
+		self._write_jokes_to_file(jokes, filename)
 
 		return jokes
 
@@ -73,7 +80,7 @@ class RedditJokeScraper(object):
 			if button is None:
 				continue
 
-			yield self._get_joke(entry, button, self.expand_attempts), domain
+			yield self._get_id(entry), self._get_joke(entry, button, self.expand_attempts), domain
 
 	def _get_joke(self, entry, button, attempts_left):
 		if attempts_left == 0:
@@ -91,6 +98,12 @@ class RedditJokeScraper(object):
 	def _get_domain(self, entry):
 		try:
 			return entry.find_element_by_xpath(".//span[@class='domain']/a").get_attribute("href")
+		except NoSuchElementException:
+			return None
+
+	def _get_id(self, entry):
+		try:
+			return entry.find_element_by_xpath("./div/p/a").get_attribute("href").split("/")[-3]
 		except NoSuchElementException:
 			return None
 
@@ -125,6 +138,21 @@ class RedditJokeScraper(object):
 		except NoSuchElementException:
 			return False
 
+	def _write_jokes_to_file(self, jokes, filename):
+		joke_id = 1
+		jokes_dict = {}
+
+		for id, premise, punchline, subreddit in jokes:
+			jokes_dict[joke_id] = {"id": id,
+								   "premise": premise,
+								   "punchline": punchline,
+								   "subreddit": subreddit}
+			joke_id += 1
+
+		file = get_env_variable("TMP_DATA_PATH") + "/reddit_raw/%s" % filename
+		with open(file, "w", encoding="utf-8") as outfile:
+			json.dump(jokes_dict, outfile, indent=4, sort_keys=True)
+
 
 def run_joke_scraper():
 	base_url = "https://www.reddit.com/r/"
@@ -132,32 +160,67 @@ def run_joke_scraper():
 				  "darkjokes", "MeanJokes", "AntiAntiJokes", "dadjokes", "ProgrammerHumor",
 				  "MathJokes", "MommaJokes", "3amjokes", "ShortCleanFunny", "badjokes",
 				  "deadbabyjokes", "DarkHumor", "Punny", "pun", "ScienceJokes", "chemistryjokes",
-				  "science_jokes", "intellectualdadjokes", "ProgrammerDadJokes", "nsfwdadjokes",
+				  "intellectualdadjokes", "ProgrammerDadJokes", "nsfwdadjokes",
 				  "dadjokesinhistory", "Hearthstonedadjokes", "dadsouls", "warcraftdadjokes",
 				  "dota2dadjokes", "DestinyDadJokes", "FFXIVDadjokes", "Falloutdadjokes", "DMDadJokes",
 				  "skyrimdadjokes", "OverwatchDadjokes", "DarkDadJokes", "CivDadJokes", "TrahearneJokes",
 				  "StarWarsDadJokes", "eu4dadjokes", "shubreddit", "momjokes"]
-	start_urls = [base_url + subreddit for subreddit in subreddits]
+
+	subreddits_scraped = {filename.split(".")[0] for filename in os.listdir("../data/reddit_raw")}
+
+	start_urls = [base_url + subreddit for subreddit in subreddits if subreddit not in subreddits_scraped]
 
 	scraper = RedditJokeScraper(subreddits, 1000)
-	jokes = Parallel(n_jobs=-1)(delayed(scraper.scrape)(start_url) for start_url in start_urls[-1:])
+	jokes = Parallel(n_jobs=1)(delayed(scraper.scrape)(start_url) for start_url in start_urls)
 
-	formatted_jokes = {}
-	joke_id = 1
 
-	for subreddit_jokes in jokes:
-		for premise, punchline, subreddit in subreddit_jokes:
-			formatted_jokes[joke_id] = {"premise": premise,
-										"punchline": punchline,
-										"subreddit": subreddit}
-			joke_id += 1
+# formatted_jokes = {}
+# joke_id = 1
+#
+# for subreddit_jokes in jokes:
+# 	for id, premise, punchline, subreddit in subreddit_jokes:
+# 		formatted_jokes[joke_id] = {"id": id,
+# 									"premise": premise,
+# 									"punchline": punchline,
+# 									"subreddit": subreddit}
+# 		joke_id += 1
+#
+# with open("../data/jokes.json", "w", encoding="utf-8") as outfile:
+# 	json.dump(formatted_jokes, outfile, indent=4, sort_keys=True)
 
-	with open("../data/jokes.json", "w", encoding="utf-8") as outfile:
-		json.dump(formatted_jokes, outfile, indent=4, sort_keys=True)
+def get_reddit_joke_files():
+	directory = get_env_variable("TMP_DATA_PATH") + "/reddit_raw"
+	for root, dirs, files in os.walk(directory):
+		for file in files:
+			if file.endswith(".json"):
+				yield os.path.join(root, file)
+
+
+def get_all_reddit_jokes():
+	jokes = {}
+
+	for file in get_reddit_joke_files():
+		with open(file, "r") as infile:
+			data = json.load(infile)
+
+		for key, value in data.items():
+			id = value["id"]
+			premise = value["premise"]
+			punchline = value["punchline"]
+			subreddit = value["subreddit"]
+
+			jokes[id] = {"premise": premise,
+						 "punchline": punchline,
+						 "subreddit": subreddit}
+
+	print(len(jokes))
+
+	return jokes
 
 
 def main():
-	run_joke_scraper()
+	# run_joke_scraper()
+	get_all_reddit_jokes()
 
 
 # with open("../data/jokes.json", "r") as infile:
